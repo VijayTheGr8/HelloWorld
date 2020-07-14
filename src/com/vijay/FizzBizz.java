4c4eec78695a6e19642d12eca546aaa3ba5c728d
vijay <vijayspatnaik@gmail.com>
2020-07-14 10:55:48 -0400

    Fixing back

--- src/com/vijay/FizzBizz.java
+++ src/com/vijay/FizzBizz.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 
 public class FizzBizz {
     public static void main(String[] args) {
-        // buzzzzzzzzzzzzzzzz
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Number: ");
