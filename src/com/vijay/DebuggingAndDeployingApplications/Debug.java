3669427c1a4c906e5e71648867b38b788b3f8f5b
vijay <vijayspatnaik@gmail.com>
2020-07-16 11:12:47 -0400

    refined the MortgageCalculator

---
+++ src/com/vijay/DebuggingAndDeployingApplications/Debug.java
@@ -0,0 +1,14 @@
+package com.vijay.DebuggingAndDeployingApplications;
+
+public class Debug {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        printNumbers(4);
+        System.out.println("Finish");
+    }
+
+    public static void printNumbers(int limit) {
+        for(int i = 0; i <= limit; i++)
+            System.out.println(i);
+    }
+}
---
+++ src/com/vijay/DebuggingAndDeployingApplications/Deploy.java
@@ -0,0 +1,7 @@
+package com.vijay.DebuggingAndDeployingApplications;
+
+public class Deploy {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
