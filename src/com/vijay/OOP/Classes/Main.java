eafabafc34b1c2909fb5786201a160e5e6dab89b
vijay <vijayspatnaik@gmail.com>
2020-07-17 16:02:36 -0400

    learned about memory allocation

---
+++ src/com/vijay/OOP/Classes/Main.java
@@ -0,0 +1,15 @@
+package com.vijay.OOP.Classes;
+
+import org.w3c.dom.Text;
+
+public class Main {
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text.toUpperCase());
+
+        var textBox2 = new TextBox();
+        textBox2.setText("Box 2");
+        System.out.println(textBox2.text);
+    }
+}
