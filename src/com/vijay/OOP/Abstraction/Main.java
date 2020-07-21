19b94b30ec2fc21118fbc5964312b98de44fbc2f
VijayTheGr8 <vijayspatnaik@gmail.com>
2020-07-21 12:06:44 -0400

    abstraction and constructors

---
+++ src/com/vijay/OOP/Abstraction/Main.java
@@ -0,0 +1,7 @@
+package com.vijay.OOP.Abstraction;
+
+public class Main {
+    public static void main(String[] args) {
+        var browser = new Browser();
+    }
+}
--- src/com/vijay/OOP/Encapsulation/Main.java
+++ src/com/vijay/OOP/Encapsulation/Main.java
@@ -2,9 +2,10 @@ package com.vijay.OOP.Encapsulation;
 
 public class Main {
     public static void main(String[] args) {
-        var employee = new Employee();
-        employee.setBaseSalary(50_000);
-        employee.setHourlyRate(20);
+        var employee = new Employee(
+                50_000, 20);
+//        employee.setBaseSalary(50_000);
+//        employee.setHourlyRate(20);
         int wage = employee.calculateWage(10);
         System.out.println(wage);
     }
