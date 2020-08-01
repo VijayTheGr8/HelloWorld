64e960b2de180ca60ffb4514cb3ce443f14156e1
VijayTheGr8 <vijayspatnaik@gmail.com>
2020-08-01 11:28:12 -0400

    race condition

---
+++ src/com/vijay/advancedTopics/concurrencyAndMultiThreading/DownloadStatus.java
@@ -0,0 +1,13 @@
+package com.vijay.advancedTopics.concurrencyAndMultiThreading;
+
+public class DownloadStatus {
+    private int totalBytes;
+
+    public int getTotalBytes() {
+        return totalBytes;
+    }
+
+    public void incrementTotalBytes() {
+        totalBytes++;
+    }
+}
--- src/com/vijay/advancedTopics/concurrencyAndMultiThreading/ThreadDemo.java
+++ src/com/vijay/advancedTopics/concurrencyAndMultiThreading/ThreadDemo.java
@@ -1,14 +1,32 @@
 package com.vijay.advancedTopics.concurrencyAndMultiThreading;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ThreadDemo {
     public static void show() {
 
-        System.out.println(Thread.currentThread().getName());
+        var status = new DownloadStatus();
 
+        List<Thread> threads = new ArrayList<>();
+
+        // start all threads
         for (var i = 0; i < 10; i++) {
-            Thread thread = new Thread(new DownloadFileTask());
+            var thread = new Thread(new DownloadFileTask(status));
             thread.start();
+            threads.add(thread);
         }
+
+        // wait for all threads to finish
+        for (var thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("\nTotal Number of Bytes read: " + status.getTotalBytes());
 
     }
 }
