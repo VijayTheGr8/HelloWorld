5bc432f291dc568d90ac8a39d920fd0917be6254
VijayTheGr8 <vijayspatnaik@gmail.com>
2020-08-02 11:30:49 -0400

    test

--- src/com/vijay/advancedTopics/concurrencyAndMultiThreading/ThreadDemo.java
+++ src/com/vijay/advancedTopics/concurrencyAndMultiThreading/ThreadDemo.java
@@ -6,7 +6,15 @@ public class ThreadDemo {
 
         var thread1 = new Thread(new DownloadFileTask(status));
         var thread2 = new Thread(() -> {
-            while(!status.isDone()) {}  // waits until thread1 is done
+            while(!status.isDone()) {
+                synchronized (status) {
+                    try {
+                        status.wait();      // sets this Thread to wait for a notification
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
             System.out.println(status.getTotalBytes());
         });
 
