d8177afe21cb08fecee6ff65ac27c5696e2ac162
VijayTheGr8 <vijayspatnaik@gmail.com>
2020-08-09 10:54:00 -0400

    really done with this Java!

---
+++ src/com/projects/advanced/FlightService.java
@@ -0,0 +1,31 @@
+package com.projects.advanced;
+
+import com.vijay.advancedTopics.executive.LongTask;
+
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class FlightService {
+
+    public Stream<CompletableFuture<Quote>> getQuotes() {
+        var sites = List.of("site1", "site2", "site3");
+
+        return sites.stream().map(this::getQuote);
+    }
+
+    public CompletableFuture<Quote> getQuote(String site) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("Getting a quote from " + site);
+
+            var random = new Random();
+
+            LongTask.simulate(1_000 + random.nextInt(2_000));
+
+            var price = 100 + random.nextInt(10);
+
+            return new Quote(site, price);
+        });
+    }
+}
